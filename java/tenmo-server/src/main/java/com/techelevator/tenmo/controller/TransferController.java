55835acea31a130c5eed6a2bca90a0cb85dc43ac
Terrell Singletary <terrellsingle359@gmail.com>
2020-11-05 11:11:23 -0500

    Cap2 driving Terrell

--- java/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -1,5 +1,26 @@
 package com.techelevator.tenmo.controller;
 
-public class TransferController {
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.techelevator.tenmo.dao.TransfersDAO;
+import com.techelevator.tenmo.model.User;
+
+@RestController
+public class TransferController {
+	private TransfersDAO transfersDAO;
+	
+	public TransferController(TransfersDAO transfersDAO)
+	{
+		this.transfersDAO = transfersDAO;
+	}
+	
+	@RequestMapping(value = "/balance", method = RequestMethod.POST)
+	public double getUserBalance(@RequestBody User user)
+	{
+		return transfersDAO.getBalance(user.getUsername());
+	}
+	
 }
